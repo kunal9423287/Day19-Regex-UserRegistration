57d35372d13379c07ee9440b7107434277d8b342
Your Name <you@example.com>
2022-05-12 15:33:30 +0530

    Added:checked entered first name valid or not

--- src/com/Bridgelabz/UserRegistration.java
+++ src/com/Bridgelabz/UserRegistration.java
@@ -1,9 +1,17 @@
 package com.Bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Regex UserRegistration program ");
+		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first Name : ");
+        String firstName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
 	}
 
 }
