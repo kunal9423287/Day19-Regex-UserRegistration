9a6719ce208ad8c9cf35c2ab9fc8e8e9d2a3f069
Your Name <you@example.com>
2022-07-07 02:26:16 +0530

    Added:should clear all

--- src/com/Bridgelabz/UserRegistration.java
+++ src/com/Bridgelabz/UserRegistration.java
@@ -60,6 +60,23 @@ public class UserRegistration {
         System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=[^@#!$%^&+=]*[@#!$%^&+=][^@#!$%^&+=]*$)(?=.*[0-9]).{8,}$", password));
         
         
+        
+        ////user need to clear all email samples provided separetely
+        String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+        String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+
+        System.out.println("Valid email :");
+        emailValidate(validEmailSample);
+        System.out.println("\nInvalid emails :");
+        emailValidate(invalidEmailSample);
+    }
+
+    public static void emailValidate(String[] emails) {
+        for(int i = 0; i< emails.length; i++ ){
+            System.out.print(Pattern.matches("^[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", emails[i]) + " ");
+        }
+        
+        
 	}
 
 }
